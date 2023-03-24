29dc388d1502a4e5da6cc8bc7ba0e6ffa8827f57
Dennisomondi <denis.omondi@student.moringaschool.com>
2023-03-24 20:04:35 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ superbowlWin = (record) => {
     const result = record.find( record => record.result === "W" );
     return !!result ? result.year : undefined;
   }
-console.log( superbowlWin(record));
+
